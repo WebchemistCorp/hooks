bc2827b72702f3b6dc6f9b4a91dca2fbb18260bf
GennYoon <yoonwonyoul@webchemist.net>
2024-02-16 21:46:39 +0900

    add example tailwindcss & add useDebounc, useThrottle

--- example/index.tsx
+++ example/index.tsx
@@ -1,20 +1,38 @@
-import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { useQueryString } from '../dist';
+import { useQueryString, useDebounce, useThrottle } from '../dist';
 
 const App = () => {
+  const [number, setNumber] = React.useState<number>(0);
+  const debounce = useDebounce();
+  const throttle = useThrottle();
   const { query, setQuery, isLoading } = useQueryString({});
-
-  console.log(query, isLoading);
 
   const handleChange = () => {
     setQuery({ test: 111, tet: 123123 });
   };
+
+  const addNumber = () => setNumber(n => n + 1);
+  const subNumber = () => setNumber(n => n - 1);
+
+  const handleDebounce = debounce(addNumber, 1000);
+  const handleThrottle = throttle(subNumber, 1000);
+
+  if (isLoading) <div>Loading ...</div>;
+
   return (
     <div>
-      <h1>HelloWorld</h1>
-      <button onClick={handleChange}>test</button>
+      <h1 className="text-xl font-bold">useQueryString</h1>
+      <p>Query : {JSON.stringify(query)}</p>
+      <button onClick={handleChange}>change query string</button>
+
+      <h1 className="text-xl font-bold">useDebounce</h1>
+      <p>{number}</p>
+      <button onClick={handleDebounce}>debounce - add</button>
+
+      <h1 className="text-xl font-bold">useThrottle</h1>
+      <p>{number}</p>
+      <button onClick={handleThrottle}>throttle - sub</button>
     </div>
   );
 };
