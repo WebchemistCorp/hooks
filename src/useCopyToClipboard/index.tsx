6b9b7eb01bf0b23e53df6aced37ba2e810ffe8a2
GennYoon <yoonwonyoul@webchemist.net>
2024-02-20 16:21:39 +0900

    hotfix 잘못된 동작 useCopyToClipboard

--- src/useCopyToClipboard/index.tsx
+++ src/useCopyToClipboard/index.tsx
@@ -1,6 +1,8 @@
 import { useRef, useState, RefObject } from 'react';
 
-export const useCopyToClipboard = <T extends HTMLElement | HTMLInputElement>(
+type InputElement = HTMLInputElement | HTMLTextAreaElement;
+
+export const useCopyToClipboard = <T extends HTMLElement | InputElement>(
   ms = 2000
 ): [RefObject<T>, () => void, boolean] => {
   const eventRef = useRef<NodeJS.Timeout>();
@@ -12,10 +14,13 @@ export const useCopyToClipboard = <T extends HTMLElement | HTMLInputElement>(
 
     setCopied(true);
     if (ref.current !== null) {
-      if (ref.current.textContent !== null) {
+      if (
+        ref.current.tagName === 'INPUT' ||
+        ref.current.tagName === 'TEXTAREA'
+      ) {
+        navigator.clipboard.writeText((ref.current as InputElement).value);
+      } else if (ref.current.textContent !== null) {
         navigator.clipboard.writeText(ref.current.textContent);
-      } else if ((ref.current as HTMLInputElement).value !== null) {
-        navigator.clipboard.writeText((ref.current as HTMLInputElement).value);
       }
     }
 
